5c2e7b814525d3a2ecb3a5717f518277bdbd8c6a
LisaScorzon <lisabailey.eu@gmail.com>
2018-04-24 15:40:40 +0200

    first commit

---
+++ server/src/games/logic.ts
@@ -0,0 +1,11 @@
+// import { ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator'
+ //import { Score, Symbol} from './entities'
+
+
+
+
+//export const calculateWinner = (score :Score)
+   // if total dice of playX >= 18- declare winner
+
+//export const finished = (score: Score)
+  // if total dice of playX >= 18- declare finished
